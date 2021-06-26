60030e8910f71678738b958a3539edf578907cdb
CHANN <chann@chann.kr>
2021-06-27 07:21:10 +0900

    :construction: Adjust scroll listener and shoddy work

--- twitterExtension.user.js
+++ twitterExtension.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter Extension
 // @namespace    https://chann.dev/
-// @version      0.1
+// @version      0.2
 // @icon https://user-images.githubusercontent.com/1831308/99884645-3101e100-2c73-11eb-9258-958dee09457e.png
 // @description  Twitter Extension
 // @author       CHANN <chann@chann.kr>
@@ -45,61 +45,92 @@ window.addEventListener('keyup', (event) => {
 // window.removeEventListener('keyup')
 
 // TEMP: hongSeonbi translator
-// TODO: Find a new way rather than after 5s
+// TODO: Find a new way rather than after 3s
 // ------------------------------------------------------------
-setTimeout(() => {
-  // const timeline = document.querySelector('.css-1dbjc4n > div > div')
-  const timeline = document.querySelector(
-    '[aria-labelledby="accessible-list-0"] > div > div'
-  )
-  console.debug('timeline', timeline)
-  const config = {
-    attributes: true,
-    childList: true,
-    // subtree: true,
-  }
-
-  // Translate seonbi's tweet
-  const hongSeonbi = (mutationsList) => {
-    // console.debug('mutationsList', mutationsList)
-    for (const mutation of mutationsList) {
-      // Find seonbi and translate if timeline updated
-      if (mutation.type === 'childList') {
-        // console.debug('mutation', mutation)
-        mutation.target.children.forEach((tweet) => {
-          const authorLink = tweet.querySelector(
-            'div > div > article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:first-child > div > div > div:first-child > div:first-child > a'
-          )
-          // console.debug('authorLink', authorLink)
-          if (authorLink) {
-            const targetUsers = [
-              'hong2tu4',
-              'channprj',
-              'CNN',
-              'resten1497',
-              'blurfxo',
-              'nameEO',
-              'haruair',
-              'shiftpsh',
-              'salgujelly',
-              'shiftpsh',
-            ]
-            let username = authorLink.getAttribute('href').split('/')[1]
-            // console.debug('username', username)
-            if (targetUsers.includes(username)) {
-              const nameElem = authorLink.querySelector(
-                'div > div:first-child > div:first-child > span > span'
-              )
-              nameElem.style.color = 'red'
-            }
-          }
-        })
-        console.debug('mutation.target', mutation.target)
-      } else if (mutation.type === 'attributes') {
-        // mutation.attributeName attribute was modified.
+const hongSeonbiTranslator = () => {
+  try {
+    setTimeout(() => {
+      const config = {
+        attributes: true,
+        childList: true,
+        // subtree: true,
       }
-    }
+
+      // TODO: Change timeline selector when it changed something
+      // const timeline = document.querySelector('.css-1dbjc4n > div > div')
+      // const timeline = document.querySelector(
+      //   '[aria-labelledby="accessible-list-0"] > div > div'
+      // )
+      const timeline = document.querySelector(
+        '[aria-label="Timeline: Your Home Timeline"] > div'
+      )
+      console.debug('timeline', timeline)
+
+      // Translate seonbi's tweet
+      const hongSeonbi = (mutationsList) => {
+        // console.debug('mutationsList', mutationsList)
+        for (const mutation of mutationsList) {
+          // Find seonbi and translate if timeline updated
+          if (mutation.type === 'childList') {
+            // console.debug('mutation', mutation)
+            mutation.target.children.forEach((tweet) => {
+              const authorLink = tweet.querySelector(
+                'div > div > article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:first-child > div > div > div:first-child > div:first-child > a'
+              )
+              // console.debug('authorLink', authorLink)
+              if (authorLink) {
+                const targetUsers = [
+                  'hong2tu4',
+                  'channprj',
+                  'BBCWorld',
+                  'CNN',
+                  'cnnbrk',
+                  'resten1497',
+                  'blurfxo',
+                  'nameEO',
+                  'haruair',
+                  'shiftpsh',
+                  'salgujelly',
+                  'shiftpsh',
+                  '_jeyraof',
+                ]
+                let username = authorLink.getAttribute('href').split('/')[1]
+                // console.debug('username', username)
+                if (targetUsers.includes(username)) {
+                  const nameElem = authorLink.querySelector(
+                    'div > div:first-child > div:first-child > span > span'
+                  )
+                  nameElem.style.color = 'red'
+                }
+              }
+            })
+            console.debug('mutation.target', mutation.target)
+          } else if (mutation.type === 'attributes') {
+            // mutation.attributeName attribute was modified.
+          }
+        }
+      }
+      const observer = new MutationObserver(hongSeonbi)
+      observer.observe(timeline, config)
+    }, 3000)
+  } catch (error) {
+    console.debug('hongSeonbiTranslator', error)
+    throw error
   }
-  const observer = new MutationObserver(hongSeonbi)
-  observer.observe(timeline, config)
-}, 5000)
+}
+
+// Apply hongSeonbiTranslator with scroll event
+hongSeonbiTranslator()
+let timer = null
+window.addEventListener(
+  'scroll',
+  (event) => {
+    if (timer !== null) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(function () {
+      hongSeonbiTranslator()
+    }, 150)
+  },
+  false
+)
