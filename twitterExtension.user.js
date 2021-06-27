fb7e7c5601acaacfb4d6fcd15cf08414d480b85e
CHANN <chann@chann.kr>
2021-06-28 03:43:53 +0900

    :construction: Add translate buttons with major bugs...

--- twitterExtension.user.js
+++ twitterExtension.user.js
@@ -66,6 +66,22 @@ window.addEventListener('keyup', (event) => {
 })
 // window.removeEventListener('keyup')
 
+// Utils
+// ------------------------------------------------------------
+const fetchRetry = (url, options = {}, retries) =>
+  fetch(url, options)
+    .then((res) => {
+      console.debug('fetchRetry', res)
+      if (res.ok) {
+        return res.json()
+      }
+      if (retries > 0) {
+        return fetchRetry(url, options, retries - 1)
+      }
+      throw new Error(res.status)
+    })
+    .catch((error) => console.error('fetchRetry:error', error.message))
+
 // TEMP: hongSeonbi translator
 // ------------------------------------------------------------
 const timelineSelector = () => {
@@ -100,38 +116,84 @@ const hongSeonbiMutation = (mutationsList) => {
       mutation.target.children.forEach((tweet) => {
         let authorSelector =
           'div > div > article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:first-child > div > div > div:first-child > div:first-child > a'
+        let contentSelector =
+          'div > div > article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:first-child > div'
         // Reassign the author selector for target user timeline
-        const targetUserTimeline = ['/hong2tu4', '/salgujelly', '/channprj']
+        const targetUserTimeline = ['/hongminhee', '/hong2tu4', '/salgujelly']
         if (targetUserTimeline.includes(document.location.pathname)) {
           authorSelector =
             'div > div > article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:first-child > div > div > div:first-child > div:first-child > a'
         }
         const authorLink = tweet.querySelector(authorSelector)
+        const content = tweet.querySelector(contentSelector)
         // console.debug('authorLink', authorLink)
         if (authorLink) {
+          let username = authorLink.getAttribute('href').split('/')[1]
+          const seonbiUser = ['hongminhee', 'hong2tu4', 'channprj']
           const targetUsers = [
+            'hongminhee',
             'hong2tu4',
             'salgujelly',
+            // Test users (night owl)
+            // --------------------
             'channprj',
-            'BBCWorld',
-            'CNN',
-            'cnnbrk',
-            'F1',
-            'resten1497',
-            'blurfxo',
-            'nameEO',
-            'haruair',
-            'shiftpsh',
-            'shiftpsh',
-            '_jeyraof',
+            // 'BBCWorld',
+            // 'CNN',
+            // 'cnnbrk',
+            // 'F1',
+            // 'resten1497',
+            // 'blurfxo',
+            // 'nameEO',
+            // 'haruair',
+            // 'shiftpsh',
+            // '_jeyraof',
+            // 'lqez',
+            // --------------------
           ]
-          let username = authorLink.getAttribute('href').split('/')[1]
-          // console.debug('username', username)
+          // Translate with seonbi
+          if (seonbiUser.includes(username)) {
+            // TODO: Translate with seonbi
+            // TODO: Bypass Twitter CSP
+            // let headers = new Headers()
+            // headers.append('Content-Type', 'application/json')
+            // let body = JSON.stringify({
+            //   preset: 'ko-kr',
+            //   sourceHtml: content.innerText,
+            // })
+
+            // let requestOptions = {
+            //   method: 'POST',
+            //   headers,
+            //   body,
+            //   redirect: 'follow',
+            // }
+            // fetchRetry('https://seonbi.herokuapp.com/', requestOptions, 3).then(
+            //   (response) => {
+            //     const result = response.json()
+            //     var translated = document.createElement('span')
+            //     translated.innerText = '\n\n' + result.resultHtml
+            //     content.appendChild(translated)
+            //   }
+            // )
+            // TODO: Append buttons continuously; Do not append if exists
+            var translated = document.createElement('button')
+            translated.innerText = '번역'
+            content.appendChild(translated)
+
+            content.style.color = 'blue'
+          }
+
+          // Change name and color
           if (targetUsers.includes(username)) {
-            const nameElem = authorLink.querySelector(
+            let nameElem = authorLink.querySelector(
               'div > div:first-child > div:first-child > span > span'
             )
-            nameElem.style.color = 'red'
+            if (username === 'salgujelly') {
+              nameElem.textContent = `🐰 ${username} 🎩`
+            } else {
+              nameElem.textContent = `🎩 ${username} 🐰`
+            }
+            nameElem.style.color = 'pink'
           }
           // TODO: Translate seonbi's tweet
         }
