7981e6836c53099ca66005778c31d67f46a69020
Park Hee Chan <chann@chann.kr>
2024-12-14 14:42:27 +0900

    update: fix match and name

--- twitterExtension.user.js
+++ twitterExtension.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Twitter Extension
-// @namespace    https://chann.dev/
+// @name         X Shortcut Extension
+// @namespace    https://chann.dev
 // @version      0.1
 // @icon https://user-images.githubusercontent.com/1831308/99884645-3101e100-2c73-11eb-9258-958dee09457e.png
-// @description  Twitter Extension
-// @author       CHANN <chann@chann.kr>
-// @match        http*://twitter.com/*
+// @description  X(fomerly Twitter) Extension
+// @author       CHANN <chann@chann.dev>
+// @match        https://x.com/*
 // @run-at       document-end
 // ==/UserScript==
 
